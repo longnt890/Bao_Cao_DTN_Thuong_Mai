448ac0d283625f10d602551b61e59328c466ea52
TaiNguyen14122002 <2024801030129@student.tdmu.edu.vn>
2023-12-11 21:14:33 +0700

    first commit

--- api/models/user.js
+++ api/models/user.js
@@ -1,55 +1,48 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-    idTask: {
-        type: Number,
-        require: true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+  addresses: [
+    {
+      name: String,
+      mobileNo: String,
+      houseNo: String,
+      street: String,
+      landmark: String,
+      city: String,
+      country: String,
+      postalCode: String,
     },
-    content:{
-        type: String,
-        require: true
+  ],
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
     },
-    name: {
-        type: String,
-        require:true
-    },
-    email: {
-        type: String,
-        require:true,
-        unique: true
-    },
-    password: {
-        type: String,
-        require:true
-    },
-    verified:{
-        type: Boolean,
-        default: false
-    },
-    verificationToken: String,
-    addresses: [
-        {
-            name: String,
-            mobileNo: String,
-            houseNo: String,
-            street: String,
-            landmark: String,
-            city: String,
-            country: String,
-            postalCode: String
-        }
-    ],
-    orders: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Order"
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User",userSchema);
 
 module.exports = User
